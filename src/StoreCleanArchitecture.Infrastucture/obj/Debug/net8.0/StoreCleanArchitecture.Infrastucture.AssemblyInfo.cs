fa34ddc6c194833c0a0d5dfb26dd9dd2bcb21b55
RedoTV <redotv1@gmail.com>
2024-06-15 13:41:59 +0300

    add cms to project

--- src/StoreCleanArchitecture.Infrastucture/obj/Debug/net8.0/StoreCleanArchitecture.Infrastucture.AssemblyInfo.cs
+++ src/StoreCleanArchitecture.Infrastucture/obj/Debug/net8.0/StoreCleanArchitecture.Infrastucture.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreCleanArchitecture.Infrastucture")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccfeabf0cd79804a8282e088ba64467f3bc077bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+944b4f35c9239fe9dc2ac712c35078c6d1536525")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreCleanArchitecture.Infrastucture")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreCleanArchitecture.Infrastucture")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
