d05f73177f7e38dcc7638998a7d2f8284a39ec33
RedoTV <redotv1@gmail.com>
2024-08-27 22:09:24 +0300

    making header

--- src/StoreCleanArchitecture.Presentation/store-client/src/components/Header/Header.jsx
+++ src/StoreCleanArchitecture.Presentation/store-client/src/components/Header/Header.jsx
@@ -1,47 +1,78 @@
 import "./Header.css"
+import { useState } from "react";
 // import {  } from '@headlessui/react'
-import { ShoppingBagIcon, MagnifyingGlassCircleIcon, ShoppingCartIcon } from '@heroicons/react/24/outline'
+import { ShoppingBagIcon, MagnifyingGlassCircleIcon, ShoppingCartIcon, Bars2Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 
 export default function Header() {
-
+  const [open, setOpen] = useState(false);
   return (
     <>
       <header className="bg-[#20283f] text-white 
         sticky top-0 scroll-smooth">
-        <div className="
-          grid grid-cols-2 sm:grid-cols-[1fr_minmax(400px,500px)_1fr] p-4 duration-500 
-           mx-auto m-auto"
+        <nav className="
+          grid grid-cols-2 sm:grid-cols-[1fr_minmax(200px,400px)_1fr] 
+          place-items-center p-5 duration-500 
+          gap-4"
         >
-          <a href="#" className="hover:opacity-70 transition duration-500 my-auto 
-            row-start-1 col-start-1 w-32"
+          <a href="#" className="hover:opacity-70 transition duration-500
+            row-start-1 col-start-1 w-max inline-flex sm:m-0 mr-auto "
           >
-            <ShoppingBagIcon className="inline-block h-8 sm:h-10"/>
-            <span className="text-xl sm:text-2xl align-middle pl-2">Store</span>
+            <ShoppingBagIcon className="h-10 stroke-sky-500"/>
+            <div className="inline-flex items-center ml-1">
+              <div className="text-xl sm:text-2xl text-blue-100 ">
+                Store 
+              </div>
+              <span className="text-xl sm:text-2xl text-blue-300">X</span>
+            </div>
           </a>
+          
+          <div className="inline-flex ml-auto sm:m-0">
+            <a href="#" className="row-start-1 sm:col-start-3 col-start-2 
+              hover:opacity-70 duration-500"
+            >
+              <ShoppingCartIcon className="h-10"/>
+            </a>
 
-          <div className="ml-auto row-start-1 my-auto sm:col-start-3 col-start-2">
-            <ShoppingCartIcon className="
-              h-8 sm:h-10 cursor-pointer hover:opacity-70 
-              transition duration-500 "
-            />
+            <div className="sm:hidden ml-4 cursor-pointer rounded-3xl bg-blue-600" 
+              onClick={() => setOpen(!open)}
+            >
+            {open ? 
+              <XMarkIcon className="h-10"/> : 
+              <Bars2Icon className="h-10"/>
+            }
+              
+            </div>
+
           </div>
 
-          <form className="sm:my-auto relative mt-4
+          <form className="
             sm:row-start-1 sm:col-start-2 sm:col-end-2 
-             row-start-2 col-start-1 col-end-3"
+            relative col-span-2 w-full"
           >
             <input type="text" className="
-              rounded-3xl pl-5 pr-14 text-black text-xl mx-auto
-              w-full h-12"
+              rounded-3xl text-black text-xl 
+              h-12 w-full pr-14 pl-4"
             />
             <button className="h-12 w-12
-             absolute end-1 hover:opacity-80 transition"
+             absolute end-0 hover:opacity-80 transition"
             >
               <MagnifyingGlassCircleIcon className="stroke-slate-800"/>
             </button>
           </form>
-        </div>
+
+            
+          <div className={`sm:hidden flex flex-col transition-all duration-300 
+            transform ${open ? 'max-h-screen opacity-100 scale-y-100' : 
+            'max-h-0 opacity-0 scale-y-0 overflow-hidden'}`}
+          >
+            <a href="#">Home</a>
+            <a href="#">Market</a>
+            <a href="#">Home</a>
+            <a href="#">Market</a>
+          </div>
+          
+        </nav>
       </header>
     </>
   )
